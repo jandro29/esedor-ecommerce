5c2c06caab229a907111d7e5d46f13e30c06291f
Jose Carlos Colan <ascolanr@gmail.com>
2025-07-01 11:00:02 -0500

    .

--- src/app/components/selected-project/selected-project.component.ts
+++ src/app/components/selected-project/selected-project.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SelectedProjectComponent implements OnInit {
 
+
+  proyectos = [
+  {
+    nombre: 'MAP-IT',
+    descripcion: `Fundado en el 2019 en la industria del Turismo es la primera
+plataforma ecommerce de experiencias inmersivas en Perú...`,
+    servicios: 'Branding, fotografía, experiencia de usuario y desarrollo a medida.',
+    imagen: 'assets/images/mapit-logo.png',
+    link: 'https://map-it.pe'
+  },
+  {
+    nombre: 'MAP-IT',
+    descripcion: `Fundado en el 2019 en la industria del Turismo es la primera
+plataforma ecommerce de experiencias inmersivas en Perú...`,
+    servicios: 'Branding, fotografía, experiencia de usuario y desarrollo a medida.',
+    imagen: 'assets/images/mapit-logo.png',
+    link: 'https://map-it.pe'
+  },
+  {
+    nombre: 'MAP-IT',
+    descripcion: `Fundado en el 2019 en la industria del Turismo es la primera
+plataforma ecommerce de experiencias inmersivas en Perú...`,
+    servicios: 'Branding, fotografía, experiencia de usuario y desarrollo a medida.',
+    imagen: 'assets/images/mapit-logo.png',
+    link: 'https://map-it.pe'
+  },
+  // Puedes agregar más proyectos aquí
+];
+
+onScroll(event: WheelEvent) {
+  const container = document.querySelector('.horizontal-wrapper');
+  if (container) {
+    container.scrollLeft += event.deltaY;
+    event.preventDefault();
+  }
+}
+
+
   constructor() { }
 
   ngOnInit(): void {
